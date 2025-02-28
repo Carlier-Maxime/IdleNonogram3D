97d79341d7d2421068405c36f9660b907d635afa
M_EtOuais <28m06c02@gmail.com>
2025-02-28 23:02:07 +0100

    :sparkles: gen grid 2D

--- Assets/Scripts/Ore.cs
+++ Assets/Scripts/Ore.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class Ore : MonoBehaviour
 {
@@ -13,6 +12,14 @@ public class Ore : MonoBehaviour
     private void Start()
     {
         if (!cellPrefab.GetComponent<Cell>()) Debug.LogError("The Prefab used not content a component Cell.");
-        var newPrefab = Instantiate(cellPrefab, transform.position, Quaternion.identity);
+        var startPosition = transform.position - new Vector3(width / 2f, height / 2f, 0);
+        for (var x = 0; x < width; x++)
+        {
+            for (var y = 0; y < height; y++)
+            {
+                var spawnPosition = startPosition + new Vector3(x, y, 0);
+                Instantiate(cellPrefab, spawnPosition, Quaternion.identity);
+            }
+        }
     }
 }
