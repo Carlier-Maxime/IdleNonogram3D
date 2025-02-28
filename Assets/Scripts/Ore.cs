bdfc391f496ef33864b9238f8e6ccb1d1f9800b4
M_EtOuais <28m06c02@gmail.com>
2025-02-28 22:38:49 +0100

    :sparkles: Add Base of Cell and Ore

---
+++ Assets/Scripts/Ore.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using UnityEngine;
+
+public class Ore : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject cellPrefab;
+    [SerializeField]
+    private int width = 5;
+    [SerializeField]
+    private int height = 5;
+
+    private void Start()
+    {
+        if (!cellPrefab.GetComponent<Cell>()) Debug.LogError("The Prefab used not content a component Cell.");
+        var newPrefab = Instantiate(cellPrefab, transform.position, Quaternion.identity);
+    }
+}
