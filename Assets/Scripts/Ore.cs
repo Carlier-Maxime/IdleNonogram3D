00617426f2068fc97df79260367eb58cbac83722
M_EtOuais <28m06c02@gmail.com>
2025-03-02 17:42:21 +0100

    :sparkles: Gen random ore 3D

--- Assets/Scripts/Ore.cs
+++ Assets/Scripts/Ore.cs
@@ -1,4 +1,5 @@
 ﻿using System.Runtime.CompilerServices;
+using Unity.Mathematics;
 using UnityEngine;
 
 public class Ore : MonoBehaviour
@@ -9,18 +10,29 @@ public class Ore : MonoBehaviour
     private int width = 5;
     [SerializeField]
     private int height = 5;
+    [SerializeField]
+    private int depth = 5;
+    [SerializeField]
+    private float density = 0.5f;
 
     private void Start()
     {
         if (!cellPrefab.GetComponent<Cell>()) Debug.LogError("The Prefab used not content a component Cell.");
 
-        var startPosition = transform.position - new Vector3(ShiftOf(width), ShiftOf(height), 0);
+        var seed = UnityEngine.Random.Range(0, 100000);
+        Debug.Log("Ore seed : "+seed);
+        var seedOffset = new float3(seed*0.01f, seed*0.01f, seed*0.01f);
+        var startPosition = transform.position - new Vector3(ShiftOf(width), ShiftOf(height), ShiftOf(depth));
         for (var x = 0; x < width; x++)
         {
             for (var y = 0; y < height; y++)
             {
-                var spawnPosition = startPosition + new Vector3(x, y, 0);
-                Instantiate(cellPrefab, spawnPosition, Quaternion.identity, transform);
+                for (var z = 0; z < depth; z++)
+                {
+                    if ((noise.snoise(seedOffset + new float3(x, y, z))+1)*0.5f > density) continue;
+                    var spawnPosition = startPosition + new Vector3(x, y, z);
+                    Instantiate(cellPrefab, spawnPosition, Quaternion.identity, transform);
+                }
             }
         }
         return;
