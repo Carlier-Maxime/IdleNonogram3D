55949369cb07b718d26e68d4d5b47383d5280a41
M_EtOuais <28m06c02@gmail.com>
2025-03-16 13:10:48 +0100

    :sparkles: Add UnNormalizeScreenCoords

--- Assets/Scripts/Utils.cs
+++ Assets/Scripts/Utils.cs
@@ -9,4 +9,11 @@ public class Utils
         coords.y /= Screen.height;
         return coords;
     }
+
+    public static Vector2 UnNormalizeScreenCoords(Vector2 normalizedCoords)
+    {
+        normalizedCoords.x *= Screen.width;
+        normalizedCoords.y *= Screen.height;
+        return normalizedCoords;
+    }
 }
